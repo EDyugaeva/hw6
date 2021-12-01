8fc793016d5c9647ee5f5d040f4c59f2b11f67ca
Ekaterina <ed2408@yandex.ru>
2021-12-01 20:24:14 +0300

    Commit

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,74 @@
+package ru.skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        task1();
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+
+    public static void task1() {
+        int[] arr = generateRandomArray();
+//        Задача первая
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        sum = 0;
+
+        for (int i : arr) {
+            sum = sum + i;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+
+//        Задача вторая
+        int min = 200_000;
+        int max = 100_000;
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
+            }
+            if (i < min) {
+                min = i;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. " + "Максимальная сумма трат за день составила " + max + " рублей.");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. " + "Максимальная сумма трат за день составила " + max + " рублей.");
+
+
+//        Задача третья
+        float medium = (float) sum / (arr.length);
+        System.out.println("Средняя сумма трат за месяц составила " + medium + " рублей.");
+        System.out.println(arr.length);
+
+//        Задача четвертая
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        System.out.println(reverseFullName.length);
+        for (int i = reverseFullName.length-1; i >= 0 ; i--) {
+            System.out.print(reverseFullName[i]);
+
+        }
+
+
+    }
+}
